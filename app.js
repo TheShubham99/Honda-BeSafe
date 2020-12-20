71bd887d04733f19436068acc8636c978e98e429
Pratham <ssahasrabhojane@gmail.com>
2020-12-21 00:10:11 +0530

    serve html

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'content-type': 'text/html' })
+  fs.createReadStream('index.html').pipe(res)
+})
+
+server.listen(process.env.PORT || 3000)
+
 function load_data(){
    
     var name,phone,id,lon,lat,model;
