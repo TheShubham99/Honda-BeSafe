da0e3afbf5cbf8ffa46a242cf6070a30176904e2
Pratham <ssahasrabhojane@gmail.com>
2020-12-21 00:24:54 +0530

    spin an express server2

--- app.js
+++ app.js
@@ -1,43 +1,70 @@
 const http = require('http')
 const fs = require('fs')
 const express = require('express')
-
+var path = require('path');
 const app = express()
 
-app.get('/', function(req, res) {
-    res.sendFile('/index.html');
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+
+var Airtable = require('airtable');
+Airtable.configure({
+  endpointUrl: 'https://api.airtable.com',
+  apiKey: 'keyQMQW52TXceWFk5'
 });
+var base = Airtable.base('appaMvw4sxN3BYSvg');
+
+
+app.use(express.static('public'))
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + 'public/index.html'));
+});
+
+app.get('/load',function(req,res){
+    var name,ph,id,lon,lat,car;
+   
+  
+  base('Honda BeSafe').select({
+    view: 'Grid view'
+}).firstPage(function(err, records) {
+    if (err) { console.error(err); return; }
+        name=records[0].get('Name').toString();
+        car=records[0].get('Car').toString();
+        ph=records[0].get('Phone').toString();
+        lat=records[0].get('lat').toString();
+        lon=records[0].get('lon').toString()
+        id=records[0].get('id').toString()
+       
+        
+        res.send({"name":name,"car":car,"phone":ph,"lat":lat,"lon":lon,"id":id});
+});
+})
+
+
+app.get('/update',function(req,res){
+    var phone;
+    phone=req.param('ph');
+
+    console.log(phone);
+    base('Honda BeSafe').update([
+        {
+          "id": "recRaoNfvw9zNnHG9",
+          "fields": {
+            "Phone":"+"+phone 
+        }
+        }
+      ], function(err, records) {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        
+})
+})
+
 
 app.listen(process.env.PORT || 3000)
 
-/* function load_data(){
-   
-    var name,phone,id,lon,lat,model;
-   
-    fetch("http://127.0.0.1:5500/Users/ssahasrabhojane/config.json")
-    .then((res)=>{
-        res.json().then(
-            (data)=>{
-                
-                name=data['name'];
-                phone=data['phone_number'];
-                id=data['id'];
-                lon=data['lon'];
-                lat=data['lat'];
-                model=data['car_model'];
-
-            }
-        ).catch((err)=>{
-            console.log("Failed.")
-        })
-    })
-    .catch((err)=> console.log("Failed"))  
-
-
-    document.getElementById("name").value="hi";
-    console.log(name)
-
-
-}
-
- */
